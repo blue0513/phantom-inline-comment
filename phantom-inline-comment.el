fc9c420fc45a5eabadbdb2b7c4d537fa196c36ae
Taiju Aoki <aokitaiju0513@gmail.com>
2019-03-06 22:12:06 +0900

    add comments

--- phantom-inline-comment.el
+++ phantom-inline-comment.el
@@ -1,3 +1,36 @@
+;;; phantom-inline-comment --- Visible but not affect the code
+
+;; Copyright (C) 2019- blue0513
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+;; Author: blue0513
+;; URL: https://github.com/blue0513/phantom-inline-comment
+;; Version: 0.1.0
+
+;;; Commentary:
+
+;; Edit your init.el
+;;
+;; (require 'phantom-inline-comment)
+;;
+
+;;; Code:
+
+(require 'popwin)
+
 (defvar phantom-inline-comment-minor-mode-map
   (let ((kmap (make-sparse-keymap)))
     (define-key kmap (kbd "C-c C-c") 'phantom-inline-comment--apply-buffer)
@@ -5,8 +38,12 @@
     kmap))
 
 (defvar phantom-inline-comment-edit-buffer "*phantom-inline-comment-edit*")
-(defface phantom-inline-commnet-face '((t (:inherit highlight))) nil)
+
 (defvar phantom-inline-comment-state nil) ;; 'add or 'edit
+
+(defvar inline--phantom-comments nil)
+
+(defface phantom-inline-commnet-face '((t (:inherit highlight))) nil)
 
 (define-minor-mode phantom-inline-comment-minor-mode
   :init-value nil
@@ -14,9 +51,8 @@
   :keymap phantom-inline-comment-minor-mode-map
   :lighter " Phantom")
 
-(defvar inline--phantom-comments nil)
-
 (defun pic--find-overlays-specifying ()
+  "Find overlays below the cursor."
   (let ((overlays (overlays-at (point-at-eol)))
 	found)
     (while overlays
@@ -27,10 +63,12 @@
     found))
 
 (defun pic--get-overlays-after-string ()
+  "Get text property from the overlays below the cursor."
   (let* ((ovs (pic--find-overlays-specifying)))
     (overlay-get (car ovs) 'after-string)))
 
 (defun generate-inline-phantom-comment (msg)
+  "Generate overlay below the cursor with MSG."
   (pcase-let* ((pos-eol (point-at-eol))
 	       (ov (make-overlay pos-eol (1+ pos-eol)))
 	       (str (concat (when (eq pos-eol (point-max)) "\n")
@@ -41,28 +79,34 @@
     ov))
 
 (defun phantom-inline-comment--delete (phantom)
+  "Delete overlay with PHANTOM property."
   (when (overlay-get phantom 'phantom)
     (delete-overlay phantom)))
 
 (defun phantom-inline-comment--add (msg)
+  "Push overlay with MSG to the stack."
   (push (generate-inline-phantom-comment msg) inline--phantom-comments))
 
 (defun phantom-inline-comment--delete-all ()
+  "Delete all the overlays in the stack."
   (mapc #'phantom-inline-comment--delete inline--phantom-comments)
   (setq inline--phantom-comments nil))
 
 (defun phantom-inline-comment--display-edit-buffer ()
+  "Open popup-window to edit a phantom inline comment."
   (interactive)
   (popwin:popup-buffer
    (generate-new-buffer phantom-inline-comment-edit-buffer))
   (phantom-inline-comment-minor-mode 1))
 
 (defun phantom-inline-comment--delete-below ()
+  "Delete overlay after Find it."
   (interactive)
   (let* ((ovs (pic--find-overlays-specifying)))
     (phantom-inline-comment--delete (car ovs))))
 
 (defun phantom-inline-comment--apply-buffer ()
+  "Apply change in edit-buffer to the master buffer."
   (interactive)
   (let* ((str (buffer-string)))
     (popwin:close-popup-window)
@@ -73,31 +117,43 @@
 	   (phantom-inline-comment--delete-below)))))
 
 (defun phantom-inline-comment--edit-below ()
+  "Edit text on overlay and Open edit-buffer."
   (let* ((prev-comment (pic--get-overlays-after-string))
 	 (raw-prev-comment (substring-no-properties prev-comment)))
     (phantom-inline-comment--display-edit-buffer)
     (insert raw-prev-comment)))
 
 (defun phantom-inline-comment--cancel ()
+  "Close popup-window."
   (interactive)
   (popwin:close-popup-window))
 
 ;;; Main Functions
 
 (defun phantom-inline-comment-add ()
+  "Add phantom inline comment below line of the cursor."
   (interactive)
   (setq phantom-inline-comment-state 'add)
   (phantom-inline-comment--display-edit-buffer))
 
 (defun phantom-inline-comment-delete-below()
+  "Delete phantom inline comment below line of the cursor."
   (interactive)
   (phantom-inline-comment--delete-below))
 
 (defun phantom-inline-comment-delete-all ()
+  "Delete all the phantom inline comments."
   (interactive)
   (phantom-inline-comment--delete-all))
 
 (defun phantom-inline-comment-edit-below ()
+  "Edit phantom inline comment below line of the cursor."
   (interactive)
   (setq phantom-inline-comment-state 'edit)
   (phantom-inline-comment--edit-below))
+
+;; * provide
+
+(provide 'phantom-inline-comment)
+
+;;; phantom-inline-comment.el ends here
